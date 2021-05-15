2195abb62a288c71a639c2f0927187bcae3f88e5
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-15 22:16:55 +0900

    TCPSocket 코드 수정.

--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/TCPSocket.h
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/TCPSocket.h
@@ -13,6 +13,12 @@ private :
 public:
 	~TCPSocket();
 	int Connect(const SocketAddress& _addr);
+
+	int Bind(const SocketAddress& _addr);
+	int Listen(int _backLog = 32);
+	shared_ptr<TCPSocket> Accept(SocketAddress& _addr);
+	int32_t Send(const void* _data, size_t _len);
+	int32_t Receive(void* _data, size_t _len);
 };
 
 typedef shared_ptr<TCPSocket> TCPSocketPtr;

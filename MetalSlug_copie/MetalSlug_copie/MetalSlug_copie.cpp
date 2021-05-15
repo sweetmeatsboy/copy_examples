7da0b0ca21c69cb528d40f64b95b8f2bf2a4c97a
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-16 03:50:16 +0900

    코드 수정

--- MetalSlug_copie/MetalSlug_copie/MetalSlug_copie.cpp
+++ MetalSlug_copie/MetalSlug_copie/MetalSlug_copie.cpp
@@ -7,52 +7,67 @@
 #define MAX_LOADSTRING 100
 
 // 전역 변수:
-HINSTANCE hInst;                                // 현재 인스턴스입니다.
-WCHAR szTitle[MAX_LOADSTRING];                  // 제목 표시줄 텍스트입니다.
-WCHAR szWindowClass[MAX_LOADSTRING];            // 기본 창 클래스 이름입니다.
+HINSTANCE hInst;								// 현재 인스턴스입니다.
+TCHAR szTitle[MAX_LOADSTRING];					// 제목 표시줄 텍스트입니다.
+TCHAR szWindowClass[MAX_LOADSTRING];			// 기본 창 클래스 이름입니다.
 
-// 이 코드 모듈에 포함된 함수의 선언을 전달합니다:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+// 이 코드 모듈에 들어 있는 함수의 정방향 선언입니다.
+ATOM				MyRegisterClass(HINSTANCE hInstance);
+BOOL				InitInstance(HINSTANCE, int);
+LRESULT CALLBACK	WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK	About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+
+HWND g_hWnd;
+HINSTANCE g_hInst;
+
+HDC g_hdc;
+
+int APIENTRY _tWinMain(HINSTANCE hInstance,
+	HINSTANCE hPrevInstance,
+	LPTSTR    lpCmdLine,
+	int       nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: 여기에 코드를 입력합니다.
+	// TODO: 여기에 코드를 입력합니다.
+	MSG msg;
+	msg.message = WM_NULL;
 
-    // 전역 문자열을 초기화합니다.
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_METALSLUGCOPIE, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	// 전역 문자열을 초기화합니다.
+	LoadString(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadString(hInstance, IDC_NEWMETALSLUG, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // 응용 프로그램 초기화를 수행합니다:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
+	// 응용 프로그램 초기화를 수행합니다.
+	if (!InitInstance(hInstance, nCmdShow))
+	{
+		return FALSE;
+	}
 
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_METALSLUGCOPIE));
-
-    MSG msg;
-
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
+	CMainGame MainGame;
+	MainGame.Initialize();
+	DWORD dwTimer = GetTickCount();
+	// 기본 메시지 루프입니다.
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			if ((GetTickCount() - dwTimer) > SECOND / 120)
+			{
+				dwTimer = GetTickCount();
+				MainGame.Progress();
+				MainGame.Render();
+			}
+		}
+	}
+	return (int)msg.wParam;
 }
 
 
@@ -60,121 +75,104 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 //
 //  함수: MyRegisterClass()
 //
-//  용도: 창 클래스를 등록합니다.
+//  목적: 창 클래스를 등록합니다.
+//
+//  설명:
+//
+//    Windows 95에서 추가된 'RegisterClassEx' 함수보다 먼저
+//    해당 코드가 Win32 시스템과 호환되도록
+//    하려는 경우에만 이 함수를 사용합니다. 이 함수를 호출해야
+//    해당 응용 프로그램에 연결된
+//    '올바른 형식의' 작은 아이콘을 가져올 수 있습니다.
 //
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
+	WNDCLASSEX wcex;
 
-    wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.cbSize = sizeof(WNDCLASSEX);
 
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_METALSLUGCOPIE));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_METALSLUGCOPIE);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_METALSLUGCOPIE));
+	wcex.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = NULL;
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
 
-    return RegisterClassExW(&wcex);
+	return RegisterClassEx(&wcex);
 }
 
 //
 //   함수: InitInstance(HINSTANCE, int)
 //
-//   용도: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
+//   목적: 인스턴스 핸들을 저장하고 주 창을 만듭니다.
 //
-//   주석:
+//   설명:
 //
 //        이 함수를 통해 인스턴스 핸들을 전역 변수에 저장하고
 //        주 프로그램 창을 만든 다음 표시합니다.
 //
 BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
+	HWND hWnd;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
-      CW_USEDEFAULT, 0, CW_USEDEFAULT, 0, nullptr, nullptr, hInstance, nullptr);
+	RECT rc = { 0 , 0 , WINCX, WINCY };
+	AdjustWindowRect(&rc, WS_OVERLAPPEDWINDOW, FALSE);
 
-   if (!hWnd)
-   {
-      return FALSE;
-   }
+	g_hInst = hInst = hInstance; // 인스턴스 핸들을 전역 변수에 저장합니다.
 
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
+	g_hWnd = hWnd = CreateWindow(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, 0, rc.right - rc.left, rc.bottom - rc.top, NULL, NULL, hInstance, NULL);
 
-   return TRUE;
+	if (!hWnd)
+	{
+		return FALSE;
+	}
+
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+
+	return TRUE;
 }
 
 //
 //  함수: WndProc(HWND, UINT, WPARAM, LPARAM)
 //
-//  용도: 주 창의 메시지를 처리합니다.
+//  목적: 주 창의 메시지를 처리합니다.
 //
-//  WM_COMMAND  - 응용 프로그램 메뉴를 처리합니다.
-//  WM_PAINT    - 주 창을 그립니다.
-//  WM_DESTROY  - 종료 메시지를 게시하고 반환합니다.
+//  WM_COMMAND	- 응용 프로그램 메뉴를 처리합니다.
+//  WM_PAINT	- 주 창을 그립니다.
+//  WM_DESTROY	- 종료 메시지를 게시하고 반환합니다.
 //
 //
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // 메뉴 선택을 구문 분석합니다:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: 여기에 hdc를 사용하는 그리기 코드를 추가합니다...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
-}
+	int wmId, wmEvent;
 
-// 정보 대화 상자의 메시지 처리기입니다.
-INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
-{
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
-
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	switch (message)
+	{
+	case WM_COMMAND:
+		wmId = LOWORD(wParam);
+		wmEvent = HIWORD(wParam);
+		// 메뉴의 선택 영역을 구문 분석합니다.
+		break;
+	case WM_KEYDOWN:
+		switch (wParam)
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hWnd);
+			break;
+		}
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
--- MetalSlug_copie/MetalSlug_copie/MetalSlug_copie.h
+++ MetalSlug_copie/MetalSlug_copie/MetalSlug_copie.h
@@ -6,13 +6,16 @@
 #include "RTTI.h"
 #include "Obj.h"
 
+#include "Line.h"
+#include "MapMgr.h"
+
 #include "Pt.h"
 #include "Camera.h"
 #include "NormalObj.h"
 #include "Rc.h"
 
 #include "ResourceMgr.h"
-
+#include "ObjFactory.h"
 #include "BitmapDC.h"
 #include "FrameDC.h"
 #include "AniDCSet.h"
@@ -25,9 +28,74 @@
 #include "AniInfoMgr.h"
 
 #include "Device.h"
+#include "SceneMgr.h"
 
+#include "ObjMgr.h"
+
+#include "CollisionMgr.h"
 
 #include "SObj.h"
 #include "Stage_Intro.h"
 #include "Stage_1.h"
 #include "Stage_2.h"
+
+
+#include "BigCanon.h"
+#include "BossBigBullet.h"
+
+#include "BossNomalBullet.h"
+#include "SideOneTurret.h"
+#include "SideTurret.h"
+#include "Boss.h"
+
+#include "Character.h"
+#include "Soldier.h"
+#include "AtkSoldier.h"
+#include "DeadSoldier.h"
+#include "ExtraSoldier.h"
+#include "ReactionSoldier.h"
+
+#include "Player.h"
+#include "Player_1.h"
+
+#include "Helper.h"
+#include "Explosion.h"
+
+#include "BulletMgr.h"
+#include "EnemyBullet.h"
+#include "E_Bakgeok.h"
+#include "E_BallingBomb.h"
+
+#include "E_Bomb_1.h"
+#include "E_Bomb_2.h"
+
+#include "E_Mine.h"
+#include "E_NomalBullet.h"
+
+#include "PlayerBullet.h"
+#include "P_S.h"
+
+#include "P_NormalBullet.h"
+#include "P_Knife.h"
+#include "P_H.h"
+#include "P_F.h"
+#include "P_Bomb_1.h"
+
+#include "FrontGroundObject.h"
+#include "N_Item.h"
+
+#include "Door.h"
+
+#include "Rock_1.h"
+#include "W_Item.h"
+
+#include "HPBar.h"
+#include "NPCBonus.h"
+#include "TextBox.h"
+#include "WeaponBox.h"
+
+#include "UIMgr.h"
+
+#include "MainGame.h"
+
+

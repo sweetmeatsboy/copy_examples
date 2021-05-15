92c8c784efb49a1054cc1ba9c458833cc4a35d55
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-16 03:50:50 +0900

    stage 파일 추가

--- MetalSlug_copie/MetalSlug_copie/Stage_2.h
+++ MetalSlug_copie/MetalSlug_copie/Stage_2.h
@@ -1,1 +1,42 @@
 #pragma once
+class CUIMgr;
+class CCamera;
+class CLine;
+class CBoss;
+
+class CStage_2 : public CSObj
+{
+private:
+	//TurretTEST¿ë.
+	CBoss * TEST;
+private:
+	list<CObj*>* m_ObjList[OBJ_END];
+	list<CLine*>* m_Stage_1_Line_List;
+	list<CLine*>::iterator m_LinePos;
+
+	CObj* m_pPlayer;
+
+	HDC MainDC;
+	HDC m_BackBufferDC;
+	HDC m_BackGroundDC;
+	HDC m_UIMemDC;
+
+	CUIMgr* m_UI;
+	CCamera m_Cam;
+
+	int MapHeight;
+	int MapWidth;
+private:
+	void BossWave();
+public:
+	void Initialize();
+	void Progress();
+	void Render();
+	void Release();
+public:
+	CStage_2() {}
+	virtual ~CStage_2()
+	{
+		Release();
+	}
+};

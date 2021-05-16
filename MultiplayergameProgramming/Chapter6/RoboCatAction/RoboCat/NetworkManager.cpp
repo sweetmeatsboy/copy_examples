102be49432378fb8e9833632241d9947b22c26cf
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-16 16:11:57 +0900

    NetworkMgr 코드 추가중...

--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/NetworkManager.cpp
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/NetworkManager.cpp
@@ -1,1 +1,87 @@
 #include "stdafx.h"
+
+NetworkManager::NetworkManager()
+	:mBytesSentThisFrame(0)
+	, mDropPacketChance(0.f)
+	, mSimulatedLatency(0.f)
+{
+}
+
+NetworkManager::~NetworkManager()
+{
+}
+
+bool NetworkManager::Init(uint16_t _port)
+{
+	mSocket = SocketUtil::CreateUDPSocket(INET);
+	SocketAddress ownAddr(INADDR_ANY, _port);
+	mSocket->Bind(ownAddr);
+
+	LOG("%d", _port);
+
+	mBytesReceivedPerSecond = WeightedTimedMovingAverage(1.f);
+	mBytesSentPerSecond = WeightedTimedMovingAverage(1.f);
+
+	if (mSocket == nullptr)
+		return false;
+
+	if (mSocket->SetNonBlockingMode(true) != NO_ERROR)
+		return false;
+	return true;
+}
+
+void NetworkManager::ProcessIncomingPackets()
+{
+	ReadIncomingPacketsIntoQueue();
+
+	ProcessQueuedPackets();
+
+	UpdateBytesSentLastFrame();
+}
+
+void NetworkManager::ReadIncomingPacketsIntoQueue()
+{
+	char packetMem[1500];
+	int packetSize = sizeof(packetMem);
+
+	InputMemoryBitStream inputStream(packetMem, packetSize * 8);
+	SocketAddress fromAddr;
+	int receivedPacketCount = 0;
+	SocketAddress fromAddress;
+
+	int receivedPacketCount = 0;
+	int totalReadByteCount = 0;
+
+	while (receivedPacketCount < kMaxPacketsPerFrameCount)
+	{
+		int readByteCount = mSocket->ReceiveFrom(packetMem, packetSize, fromAddr);
+		if (readByteCount == 0)
+			break;
+		else if (readByteCount == -WSAEWOULDBLOCK)
+		{
+			HandleConnectinReset(fromAddr);
+		}
+		else if (readByteCount > 0)
+		{
+			inputStream.ResetToCapacity(readByteCount); 
+			++receivedPacketCount;
+			totalReadByteCount += readByteCount;
+
+			if (RoboMath::GetRandomFloat() >= mDropPacketChance)
+			{
+				float simulatedReceivedTime = Timing::sInstance.GetTimef() + mSimulatedLatency;
+				mPacketQeue.emplace(simulatedReceivedTime, inputStream, fromAddr);
+			}
+			else
+			{
+				LOG("Packet drop!", 0);
+			}
+		}
+	}
+
+	if (totalReadByteCount > 0)
+	{
+		mBytesReceivedPerSecond.UpdatePerSecond(static_cast<float>(totalReadByteCount));
+	}
+}
+
--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/NetworkManager.h
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/NetworkManager.h
@@ -1,3 +1,105 @@
 #pragma once
 
 
+typedef unordered_map<int, GameObjectPtr> IntToGameObjectMap;
+
+class NetworkManager
+{
+private:
+	class ReceivedPacket
+	{
+	private:
+		float mReceivedTime;
+		InputMemoryBitStream mPacketBuffer;
+		SocketAddress mFromAddress;
+	public:
+
+		ReceivedPacket(float _recvTime, InputMemoryBitStream& _stream);
+		const SocketAddress& GetFromAddress() const
+		{
+			return mFromAddress;
+		}
+		float GetReceivedTime() const
+		{
+			return mReceivedTime;
+		}
+		InputMemoryBitStream& GetPacketBuffer()
+		{
+			return mPacketBuffer;
+		}
+	};
+
+	float mDropPacketChance;
+	float mSimulatedLatency;
+
+	int mBytesSentThisFrame;
+	queue<ReceivedPacket, list<ReceivedPacket>> mPacketQeue;
+	UDPSocketPtr mSocket;
+
+	WeightedTimedMovingAverage mBytesReceivedPerSecond;
+	WeightedTimedMovingAverage mBytesSentPerSecond;
+
+	int mBytesSentThisFrame;
+
+	float mDropPacketChance;
+	float mSimulatedLatency;
+
+	void	UpdateBytesSentLastFrame();
+	void	ReadIncomingPacketsIntoQueue();
+	void	ProcessQueuedPackets();
+
+protected:
+	IntToGameObjectMap mNetworkIdToGameObjectMap;
+
+public:
+
+	static const uint32_t kHelloCC = 'HELO';
+	static const uint32_t kWelcomeCC = 'WLCM';
+	static const uint32_t kStateCC = 'STAT';
+	static const uint32_t kInputCC = 'INPT';
+	static const int kMaxPacketsPerFrameCount = 10;
+
+	NetworkManager();
+	virtual ~NetworkManager();
+
+	bool Init(uint16_t _port);
+	void ProcessIncomingPackets();
+
+	virtual void ProcessPacket(InputMemoryBitStream& _stream, const SocketAddress& _from) = 0;
+	virtual void HandleConnectinReset(const SocketAddress& _from)
+	{
+		(void)_from;
+	}
+	void SendPacket(const OutputMemoryBitStream& _stream, const SocketAddress& _from);
+	
+	const WeightedTimedMovingAverage& GetBytesReceivedPerSecond() const
+	{
+		return mBytesReceivedPerSecond;
+	}
+
+	const WeightedTimedMovingAverage& GetBytesSentPerSecond() const
+	{
+		return mBytesSentPerSecond;
+	}
+
+	void SetDropPacketChance(float _c)
+	{
+		mDropPacketChance = _c;
+	}
+	float GetDropPacketChance() const
+	{
+		return mDropPacketChance;
+	}
+	void SetSimulatedLatency(float _latency)
+	{
+		mSimulatedLatency = _latency;
+	}
+	float GetSimulatedLatency() const
+	{
+		return mSimulatedLatency;
+	}
+
+	inline GameObjectPtr GetGameObject(int _nId) const;
+	void AddToNetworkIdToGameObjectMap(GameObjectPtr _ptr);
+	void RemoveFromNetworkIdToGameObjectMap(GameObjectPtr _ptr);
+};

9de68b6d8d6f823ae7da14f94ef4eebc83fd6b78
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-16 15:32:03 +0900

    WeightedTimedMovingAverage 코드 추가.

--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/WeightedTimedMovingAverage.h
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/WeightedTimedMovingAverage.h
@@ -3,5 +3,48 @@
 class WeightedTimedMovingAverage
 {
 private:
+	float mTimeLastEntryMade;
+	float mValue;
+	float mDuration;
 public:
+	WeightedTimedMovingAverage(float _duration = 5.f)
+		: mDuration(_duration), mValue(0.f)
+	{
+		mTimeLastEntryMade -= Timing::sInstance.GetTimef();
+	}
+
+	void UpdatePerSecond(float _val)
+	{
+		float time = Timing::sInstance.GetTimef();
+		float timeSinceLastEntry = time - mTimeLastEntryMade;
+
+		float valueOverTime = _val / timeSinceLastEntry;
+
+		float fractionOfDuration = (timeSinceLastEntry / mDuration);
+		if (fractionOfDuration > 1.f)
+			fractionOfDuration = 1.f;
+		
+		mValue = mValue * (1.f - fractionOfDuration) + valueOverTime * fractionOfDuration;
+
+		mTimeLastEntryMade = time;
+	}
+
+	void Update(float _val)
+	{
+		float time = Timing::sInstance.GetTimef();
+		float timeSinceLastEntry = time - mTimeLastEntryMade;
+
+		float fractionOfDuration = (timeSinceLastEntry / mDuration);
+		if (fractionOfDuration > 1.f)
+			fractionOfDuration = 1.f;
+
+		mValue = mValue * (1.f - fractionOfDuration) + _val * fractionOfDuration;
+
+		mTimeLastEntryMade = time;
+	}
+
+	float GetValue() const
+	{
+		return mValue;
+	}
 };

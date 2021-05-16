2fce2cf159ea6ec11324d4b483574e5523a5ce51
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-16 18:14:07 +0900

    InputState 코드 추가

--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/InputState.h
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/InputState.h
@@ -1,1 +1,39 @@
 #pragma once
+
+class InputState
+{
+private:
+	friend class InputManager;
+
+	float mDesiredRightAmount, mDesiredLeftAmount;
+	float mDesiredForwardAmount, mDesiredBackAmount;
+	bool mIsShooting;
+
+public:
+	InputState()
+		: mDesiredBackAmount(0)
+		, mDesiredForwardAmount(0)
+		, mDesiredLeftAmount(0)
+		, mDesiredRightAmount(0)
+		, mIsShooting(false)
+	{
+	}
+
+	float GetDesiredHorizontalDelte() const
+	{
+		return mDesiredRightAmount - mDesiredLeftAmount;
+	}
+
+	float GetDesiredVerticalDelta() const
+	{
+		return mDesiredForwardAmount - mDesiredBackAmount;
+	}
+
+	bool IsShooting() const
+	{
+		return mIsShooting;
+	}
+
+	bool Write(OutputMemoryBitStream& _in) const;
+	bool Read(InputMemoryBitStream& _in);
+};

99e231e8ab1ca75005c9c5fba500809320fa220b
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-05-19 17:56:51 +0900

    no message
    (cherry picked from commit 1cd4df06014ff400404849722ede694899efdb97)

--- MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/stdafx.h
+++ MultiplayergameProgramming/Chapter6/RoboCatAction/RoboCat/stdafx.h
@@ -54,38 +54,4 @@ using std::unordered_set;
 class RoboCat;
 class GameObject;
 
-
-#include "RoboMath.h"
-#include "MemoryBitStream.h"
-#include "GameObject.h"
-#include "World.h"
-
-#include "GameObjectRegistry.h"
-
-#include "StringUtils.h"
-#include "SocketUtil.h"
-
-#include "SocketAddress.h"
-
-#include "TCPSocket.h"
-#include "UDPSocket.h"
-
-#include "SocketAddressFactory.h"
-#include "Timing.h"
-
-#include "WeightedTimedMovingAverage.h"
-
-#include "NetworkManager.h"
-#include "InputState.h"
-#include "InputAction.h"
-
-#include "Mouse.h"
-#include "Move.h"
-#include "MoveList.h"
-
-#include "RoboCat.h"
-#include "ScoreBoardManager.h"
-
-#include "Yarn.h"
-#include "Engine.h"
-#include "ReplicationCommand.h"
+#include "RoboCatShared.h"
